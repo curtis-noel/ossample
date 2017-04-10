d0f4a5560cddcd37f77152430a20603bb41d8702
yhippa <richard.yhip@gmail.com>
2017-04-10 13:59:25 -0400

    added jhipster-jdl.jh to project and generated entities from that file

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,13 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { OssampleDepartmentModule } from './department/department.module';
+import { OssampleEmployeeModule } from './employee/employee.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        OssampleDepartmentModule,
+        OssampleEmployeeModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
